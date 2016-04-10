9950c7ae5b252d136d2813bb2afdb020d36fdc42
AK101111 <arnavkansal@gmail.com>
2016-04-11 02:25:11 +0530

    Add Game entry screen

--- src/src/introScreen.java
+++ src/src/introScreen.java
@@ -1,7 +1,126 @@
 package src;
 
-/**
- * Created by arnavkansal on 11/04/16.
- */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.plaf.LayerUI;
+
+import static src.constants.WINDOW_XSIZE;
+import static src.constants.WINDOW_YSIZE;
+// some of source taken from docs.oracle.com/javase/
+// gradient idea copied
 public class introScreen {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createUI();
+            }
+        });
+    }
+
+    public static void createUI() {
+        JFrame f = new JFrame ("Ping Pong");
+
+        LayerUI<JPanel> layerUI = new SpotlightLayerUI();
+        JPanel panel = createPanel();
+        JLayer<JPanel> jlayer = new JLayer<JPanel>(panel, layerUI);
+
+        f.add (jlayer);
+
+        f.setSize(WINDOW_XSIZE,WINDOW_YSIZE);
+        f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo (null);
+        f.setVisible (true);
+    }
+
+    private static JPanel createPanel() {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel northPanel = new JPanel();
+        JPanel southPanel = new JPanel();
+        ButtonGroup entreeGroup = new ButtonGroup();
+        JRadioButton radioButton;
+        northPanel.add(radioButton = new JRadioButton("Easy", true));
+        entreeGroup.add(radioButton);
+        northPanel.add(radioButton = new JRadioButton("Hard"));
+        entreeGroup.add(radioButton);
+        northPanel.add(radioButton = new JRadioButton("Insane"));
+        entreeGroup.add(radioButton);
+
+        JButton orderButton = new JButton("Start");
+        //orderButton.setLocation(WINDOW_XSIZE/2,WINDOW_YSIZE/2);
+        //orderButton.setBounds(WINDOW_XSIZE/2,WINDOW_YSIZE/2,50,50);
+        orderButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String[] args = {};
+                PingPong.main(args);
+                //super.mouseClicked(e);
+            }
+        });
+        southPanel.add(orderButton);
+        mainPanel.add(northPanel,BorderLayout.NORTH);
+        mainPanel.add(southPanel,BorderLayout.SOUTH);
+        return mainPanel;
+    }
+}
+
+class SpotlightLayerUI extends LayerUI<JPanel> {
+    private boolean mActive;
+    private int mX, mY;
+
+    @Override
+    public void installUI(JComponent c) {
+        super.installUI(c);
+        JLayer jlayer = (JLayer)c;
+        jlayer.setLayerEventMask(
+                AWTEvent.MOUSE_EVENT_MASK |
+                        AWTEvent.MOUSE_MOTION_EVENT_MASK
+        );
+    }
+
+    @Override
+    public void uninstallUI(JComponent c) {
+        JLayer jlayer = (JLayer)c;
+        jlayer.setLayerEventMask(0);
+        super.uninstallUI(c);
+    }
+
+    @Override
+    public void paint (Graphics g, JComponent c) {
+        Graphics2D g2 = (Graphics2D)g.create();
+
+        // Paint the view.
+        super.paint (g2, c);
+
+        if (mActive) {
+            // Create a radial gradient, transparent in the middle.
+            java.awt.geom.Point2D center = new java.awt.geom.Point2D.Float(mX, mY);
+            float radius = 15;
+            float[] dist = {0.7f, 1.0f};
+            Color[] colors = {new Color(0.0f, 0.0f, 0.0f, 0.0f), Color.BLACK};
+            RadialGradientPaint p =
+                    new RadialGradientPaint(center, radius, dist, colors);
+            g2.setPaint(p);
+            g2.setComposite(AlphaComposite.getInstance(
+                    AlphaComposite.SRC_OVER, 1.0f));
+            g2.fillRect(0, 0, c.getWidth(), c.getHeight());
+        }
+
+        g2.dispose();
+    }
+
+    @Override
+    protected void processMouseEvent(MouseEvent e, JLayer l) {
+        if (e.getID() == MouseEvent.MOUSE_ENTERED) mActive = true;
+        if (e.getID() == MouseEvent.MOUSE_EXITED) mActive = false;
+        l.repaint();
+    }
+
+    @Override
+    protected void processMouseMotionEvent(MouseEvent e, JLayer l) {
+        Point p = SwingUtilities.convertPoint(e.getComponent(), e.getPoint(), l);
+        mX = p.x;
+        mY = p.y;
+        l.repaint();
+    }
 }
